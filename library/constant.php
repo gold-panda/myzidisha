c78ea1dc60d2576710dffa68bd1483c33d3b9dd2
jkurnia <julia@zidisha.org>
2014-01-08 09:17:15 -0800

    current version of library/constant file from website

--- library/constant.php
+++ library/constant.php
@@ -211,10 +211,14 @@ define("RESENDINGDAYS", 30); // send reminder for invites after these many days
 define("EXTEND_PERIOD_MULTIPLY",6);
 define("RECAPCHA_PUBLIC_KEY","6Lej9M0SAAAAAJ6LQfRiraYHaa_t_QFpLDrOOv5j");
 define("RECAPCHA_PRIVATE_KEY","6Lej9M0SAAAAAEgOXV_21hTUzoYRlckoeANlMHhc");
+
 // THIS NEEDS TO BE FALSE FOR LOGINS TO WORK LOCALLY.
 // In theory, it is set as false in constant_local.php, but you cannot define something twice.
 // So the true value remains, and thus there is the bug on the local environment.
-define("COOKIE_SECURE", $_SERVER["ENV_COOKIE_SECURE"]);
+
+define("COOKIE_SECURE", $_SERVER["ENV_COOKIE_SECURE"]); 
+//define("COOKIE_SECURE", true); 
+
 define("MAIL_CHIMP_API_KEY","78d5f8a7b16869bcdedad6eb696435e6-us1");
 define("MAIL_CHIMP_LIST_ID","d2aeae704b");
 define("MAIL_CHIMP_EMAIL","julia@zidisha.org");
