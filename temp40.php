810216f82e0ba3a245ef2bb6b19e1a0457d9f402
jkurnia <julia@zidisha.org>
2014-01-11 14:03:20 -0800

    removed unused temporary files

--- temp40.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include("library/session.php");
-	global $database,$session;
-	$test = $database->getRepayment_InstructionsByCountryCode('NE');
-	echo $test['description'];exit;
-?>
--- temp41.php
+++
@@ -1,8 +0,0 @@
-<?php
-	error_reporting(E_ALL);
-	include("library/session.php");
-	global $database,$session;
-	$q="ALTER TABLE `borrowers_extn`  ADD `late_repayment_reminder` INT NULL DEFAULT NULL,  ADD `late_repayment_reminders` INT NULL DEFAULT NULL";
-	$result=$db->query($q);
-	echo $result;
-?>
