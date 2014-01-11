59806a664c0365e0ef6305990b4d2b91e97c5a1c
jkurnia <julia@zidisha.org>
2014-01-11 08:07:45 -0800

    removed unused extra files

--- active-b.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lang['active-b']['not_allowed ']='You are not allowed to view this page.';
-$lang['active-b']['no_bussiness']='There is no business.';
-$lang['active-b']['Edit_Cmt']='Edit Comments.';
-?>
