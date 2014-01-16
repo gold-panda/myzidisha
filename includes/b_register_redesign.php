f7f8046e49ba8725876d164b208d25a6f0eb73d7
GoranStoyanov <goran@bgleads.com>
2014-01-16 18:46:14 +0200

    Remove Captcha from borrower registration form.

--- includes/b_register_redesign.php
+++ includes/b_register_redesign.php
@@ -527,7 +527,7 @@ if(empty($session->userid)){
 					?>
 				</div>
 
-				<div>
+				<div style="display:none;">
 					<label><?php echo $lang['register']['capacha'];?></label>
 					<div style="margin-top:20px"><?php echo  recaptcha_get_html(RECAPCHA_PUBLIC_KEY, $form->error("user_guess")); ?></div>
 					<a id="recaptcha_response_fielderr"></a>
