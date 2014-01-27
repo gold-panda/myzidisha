faa2528d535688cd91ee644ca3e4f70fa7fd6aca
GoranStoyanov <goran@bgleads.com>
2014-01-27 17:31:14 +0200

    The main logic of Facebook login.

--- includes/fb_login.php
+++ includes/fb_login.php
@@ -3,8 +3,26 @@
 	include_once ('../facebook/facebook.php');
 
 	$facebook = new Facebook(array('appId'  => FB_APP_ID,'secret' => FB_APP_SECRET));
-	echo '<a href="'.$facebook->getLoginUrl().'">Login with facebook</a>';
+	
+	// login url
+	echo '<a href="'.$facebook->getLoginUrl(array('redirect_uri' => SITE_URL.'includes/fb_login.php')).'">Login with facebook</a>';
+	
+	// get user id
 	$uid = $facebook->getUser();
-	echo $uid;
-	//$check_user = IsFacebookIdExist
+	
+	// check whether user exist in database
+	$check_user = $database->IsFacebookIdExist($uid);
+	
+	// if user exist
+	if($check_user)
+	{
+		// todo
+		echo '<br />User exist';
+	}
+	// if user doesn't exist
+	else
+	{
+		// todo
+		echo '<br />User doesn\'t exist';
+	}
 ?>
